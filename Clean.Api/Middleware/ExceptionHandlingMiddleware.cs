ebb97955772e112ae7e1cefd6fe5d1279308e496
parivesh.thulung <pariveshthulung@gmail.com>
2025-01-18 18:15:10 +0545

    fix: ExceptionHandlingMiddleware

--- Clean.Api/Middleware/ExceptionHandlingMiddleware.cs
+++ Clean.Api/Middleware/ExceptionHandlingMiddleware.cs
@@ -5,9 +5,9 @@ namespace Clean.Api.Middleware;
 
 public class ExceptionHandlingMiddleware
 {
-    private readonly RequestDelegate _next
+    private readonly RequestDelegate _next;
     private ILogger<ExceptionHandlingMiddleware> _logger;
-public ExceptionHandlingMiddleware(RequestDelegate next,ILogger<ExceptionHandlingMiddleware> logger)
+    public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
     {
         _next = next;
         _logger = logger;
@@ -15,9 +15,12 @@ public ExceptionHandlingMiddleware(RequestDelegate next,ILogger<ExceptionHandlin
 
     public async Task Invoke(HttpContext context)
     {
-        try{
+        try
+        {
             await _next(context);
-        }catch(Exception ex){
+        }
+        catch (Exception ex)
+        {
             _logger.LogError(ex, "Exception occured: {message}", ex.Message);
             var problemDetails = new ProblemDetails
             {
@@ -27,7 +30,7 @@ public ExceptionHandlingMiddleware(RequestDelegate next,ILogger<ExceptionHandlin
                 Extensions = new Dictionary<string, object?>
                 {
                     {"errors",new {fieldName = "Error" , descriptions ="An unexpected error occurred. Please try again later."}}
-                } 
+                }
             };
             context.Response.StatusCode = StatusCodes.Status500InternalServerError;
             await context.Response.WriteAsJsonAsync(problemDetails);
